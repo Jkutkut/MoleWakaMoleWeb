5a1170b8b8ca26dc123e936c43ebef60528195d8
Jkutkut <jorgeregonzalez@gmail.com>
2022-10-26 08:19:56 +0200

    let's work with express to implement the backend

---
+++ mole/app.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const express = require('express');
+// var session = require('express-session');
+// var passport = require('passport');
+
+var app = express();
---
+++ mole/run.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+PORT=80 npm start
