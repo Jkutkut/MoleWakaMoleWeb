7d531bb9bac9f018d033ba881e2f3b8d51d70f30
jkutkut <jorgeregonzalez@gmail.com>
2022-10-27 21:29:30 +0200

    login page stable

--- mole/routes/index.js
+++ mole/routes/index.js
@@ -7,7 +7,8 @@ var router = express.Router();
 router.get('/',
     (req, res, next) => {
         if (!req.user) {
-            return res.render('login');
+            // TODO add correct login url
+            return res.render('login', {API_LOGIN_URL: "https://github.com/jkutkut"});
         }
         next();
     },
