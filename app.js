a557d976a5570534d5acca9aaf71ec7c1ad1c87b
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-03 22:51:33 +0100

    able to retrieve the token

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const express = require('express');
 require('fetch');
 const path = require('path');
 require('dotenv').config();
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+// const logger = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const bodyParser = require('body-parser');
 
 // **** Passport ****
-const passport = require('passport');
-const FortyTwoStrategy = require('passport-42').Strategy;
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+// const passport = require('passport');
+// const FortyTwoStrategy = require('passport-42').Strategy;
+// const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 // ********** Code **********
 
@@ -24,22 +24,22 @@ const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
-passport.use(new FortyTwoStrategy(
-    {
-        clientID: process.env['CLIENT_ID'],
-        clientSecret: process.env['SECRET'],
-        callbackURL: process.env['CALLBACK_URL']
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        // In this example, the user's 42 profile is supplied as the user
-        // record.  In a production-quality application, the 42 profile should
-        // be associated with a user record in the application's database, which
-        // allows for account linking and authentication with other identity
-        // providers.
-        // console.log(profile); // TODO debug
-        return cb(null, profile);
-    }
-));
+// passport.use(new FortyTwoStrategy(
+//     {
+//         clientID: process.env['CLIENT_ID'],
+//         clientSecret: process.env['SECRET'],
+//         callbackURL: process.env['CALLBACK_URL']
+//     },
+//     function(accessToken, refreshToken, profile, cb) {
+//         // In this example, the user's 42 profile is supplied as the user
+//         // record.  In a production-quality application, the 42 profile should
+//         // be associated with a user record in the application's database, which
+//         // allows for account linking and authentication with other identity
+//         // providers.
+//         // console.log(profile); // TODO debug
+//         return cb(null, profile);
+//     }
+// ));
 
 // **** Passport session persistence ****
 // Configure Passport authenticated session persistence.
@@ -51,13 +51,13 @@ passport.use(new FortyTwoStrategy(
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete 42 profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
-    cb(null, user);
-});
+// passport.serializeUser(function(user, cb) {
+//     cb(null, user);
+// });
 
-passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-});
+// passport.deserializeUser(function(obj, cb) {
+//     cb(null, obj);
+// });
 
 // **** Express ****
 const app = express();
@@ -69,73 +69,73 @@ app.set('view engine', 'hbs');
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(logger('dev'));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-// TODO secret?
-app.use(session({ resave: false, saveUninitialized: false, secret: '!terceS' }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// // TODO secret?
+// app.use(session({ resave: false, saveUninitialized: false, secret: '!terceS' }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // **** Session ****
 // Initialize Passport and restore authentication state, if any, from the
 // session.
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
 // ********** Routes **********
 app.get(
     '/',
-    ensureLoggedIn(),
+    // ensureLoggedIn(),
     (req, res) => {
         res.redirect('/app');
     }
 );
-app.get(
-    '/login',
-    passport.authenticate('42')
-);
+// app.get(
+//     '/login',
+//     passport.authenticate('42')
+// );
 
-// TODO if login fails, loop may occur
-app.get(
-    '/callback',
-    passport.authenticate('42',
-        {
-            failureRedirect: '/login',
-            // successRedirect: '/app'
-        }
-    ),
-    async (req, res) => {
-        // TODO
-        console.log("code: " + req.query.code);
+// // TODO if login fails, loop may occur
+// app.get(
+//     '/callback',
+//     passport.authenticate('42',
+//         {
+//             failureRedirect: '/login',
+//             // successRedirect: '/app'
+//         }
+//     ),
+//     async (req, res) => {
+//         // TODO
+//         console.log("code: " + req.query.code);
 
-        let token = await fetch(
-            process.env.AUTH_URL,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    grant_type: 'authorization_code',
-                    client_id: process.env.CLIENT_ID,
-                    client_secret: process.env.SECRET,
-                    code: req.query.code,
-                    redirect_uri: process.env.CALLBACK_URL,
-                })
-            }
-        ).then(res => res.json()).then((data) => {
-            console.log(data);
-            res.redirect('/app');
-        });
-        // TODO
-        // res.redirect('/app');
-    }
-);
+//         let token = await fetch(
+//             process.env.AUTH_URL,
+//             {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({
+//                     grant_type: 'authorization_code',
+//                     client_id: process.env.CLIENT_ID,
+//                     client_secret: process.env.SECRET,
+//                     code: req.query.code,
+//                     redirect_uri: process.env.CALLBACK_URL,
+//                 })
+//             }
+//         ).then(res => res.json()).then((data) => {
+//             console.log(data);
+//             res.redirect('/app');
+//         });
+//         // TODO
+//         // res.redirect('/app');
+//     }
+// );
 
 app.get(
     '/app',
-    ensureLoggedIn(),
+    // ensureLoggedIn(),
     (req, res) => {
         res.render('app', { user: req.user });
     }
@@ -147,6 +147,28 @@ app.get(
         res.render('terminal', { user: req.user });
     }
 )
+
+var token = null;
+async function getToken() {
+    token = await fetch(
+        process.env.AUTH_URL,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                grant_type: 'client_credentials',
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.SECRET
+            })
+        }
+    ).then(res => res.json()).then((data) => {
+        console.log(data);
+        return data;
+    });
+}; getToken();
+
 
 // **** Errors ****
 // catch 404 and forward to error handler
