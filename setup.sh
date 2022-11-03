ee6081470fabce20bdad23e1f2b96b2b708eea56
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-03 08:21:24 +0100

    setup updated

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ read -p "Client ID: " CLIENT_ID
 read -p "Client secret: " SECRET
 
 {
-	echo "CALLBACK_URL=$CALLBACK_URL"
+	echo "API=https://api.intra.42.fr"
 	echo "CLIENT_ID=$CLIENT_ID"
 	echo "SECRET=$SECRET"
+	echo "CALLBACK_URL=$CALLBACK_URL"
+	echo "AUTH_URL=https://api.intra.42.fr/oauth/token"
 } > .env
