af64396bc0cff60a7bcd9b5046fbb6051291ca77
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-05 10:21:30 +0100

    working on general logic to reduce redundant code

--- public/js/cmdController.js
+++ public/js/cmdController.js
@@ -10,23 +10,45 @@ function handleCmd(command) {
     if (command === "")
         return "";
 
-    let cmd = command.split(" ");
-    switch (cmd[0]) {
-        case "help":
-            return handleHistory(cmd);
-        case "clear":
-            resultTerm.innerHTML = "";
-            return "";
-        default:
-            return "<error>Error</error>: Command not found";
+    let cmd_arr = command.split(" ");
+    let cmd = getCmd(cmd_arr[0]);
+    if (!cmd)
+        return `<error>Error</error>: Command '${cmd_arr[0]}' not found`;
+    if (!validCmd(cmd_arr, cmd))
+        return 'TODO usage'
+        // return usage(cmd);
+    if (!cmd['api']) {
+        if (cmd['cmd'] == 'help')
+            return handleHelp(cmd_arr, cmd);
+        else
+            return handleClear(cmd_arr, cmd)
     }
+    return 'api';
 }
 
-function handleHistory(cmd) {
-    if (!CMDS)
-        throw new Error("CMDS not loaded");
+function handleClear(cmd_arr, cmd) {
+    resultTerm.innerHTML = "";
+    return "";
+}
 
+function handleHelp(cmd_arr, cmd) {
     CMDS.cmds.forEach(cmd => console.log(cmd));
-
     return "<error>Error</error>: TODO";
+}
+
+// TOOLS
+function getCmd(cmd) {
+    for (let i = 0, alias; i < CMDS.cmds.length; i++) {
+        for (alias of CMDS.cmds[i]['alias']) {
+            if (alias === cmd)
+                return CMDS.cmds[i];
+        }
+    }
+    return null;
+}
+
+function validCmd(cmd_arr, cmd) {
+    // Check cmd is valid alias?
+    if (cmd['value'] )
+    return true;
 }
