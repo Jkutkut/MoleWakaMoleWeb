5c2b1b31cad53dd078553eb0f1f12289b07916e3
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-05 12:40:03 +0100

    description of commands implemented

--- public/js/cmdController.js
+++ public/js/cmdController.js
@@ -3,7 +3,14 @@ var CMDS = null;
 window.addEventListener('load', async () => {
     let response = await fetch('/json/mole.json');
     CMDS = await response.json();
+
+    // TODO debug
+    executeCommand('help');
+    executeCommand('clear');
+    executeCommand('location');
 });
+
+// ********** handle cmd **********
 
 function handleCmd(command) {
     command = command.trim();
@@ -15,8 +22,7 @@ function handleCmd(command) {
     if (!cmd)
         return `<error>Error</error>: Command '${cmd_arr[0]}' not found`;
     if (!validCmd(cmd_arr, cmd))
-        return 'TODO usage'
-        // return usage(cmd);
+        return descriptionCmd(cmd);
     if (!cmd['api']) {
         if (cmd['cmd'] == 'help')
             return handleHelp(cmd_arr, cmd);
@@ -26,6 +32,9 @@ function handleCmd(command) {
     return 'api';
 }
 
+// ********** cmds **********
+
+
 function handleClear(cmd_arr, cmd) {
     resultTerm.innerHTML = "";
     return "";
@@ -33,10 +42,11 @@ function handleClear(cmd_arr, cmd) {
 
 function handleHelp(cmd_arr, cmd) {
     CMDS.cmds.forEach(cmd => console.log(cmd));
-    return "<error>Error</error>: TODO";
+    return "<error>Error</error>: TODO"; // TODO
 }
 
-// TOOLS
+// ********** cmd tools **********
+
 function getCmd(cmd) {
     for (let i = 0, alias; i < CMDS.cmds.length; i++) {
         for (alias of CMDS.cmds[i]['alias']) {
@@ -49,6 +59,79 @@ function getCmd(cmd) {
 
 function validCmd(cmd_arr, cmd) {
     // Check cmd is valid alias?
-    if (cmd['value'] )
-    return true;
+    // if (cmd['value'] )
+    // TODO
+    // return true;
+    return false;
+}
+
+// ********** cmd format **********
+
+function descriptionCmd(cmd, fullUsage = false) {
+    let usage = `<string>${cmd['cmd']}</string>\n`;
+    usage += `  <cmd>$></cmd> ${usageCmd(cmd)}\n`;
+    if (fullUsage) {
+        usage += `  Description:\n    ${cmd['desc']}\n`;
+
+        if (cmd['flags'].length > 0) {
+            usage += `  Flags:\n`;
+            for (let flag of cmd['flags']) {
+                usage += `    ${flagFormat(flag, optionalFormat = false)}\n`;
+                usage += `      ${flag['desc']}\n`;
+            }
+        }
+        if (cmd['alias'].length > 1) {
+            usage += `  Aliases: ${cmd['alias'].join(', ')}\n`;
+        }
+    }
+    return usage;
+}
+
+function usageCmd(cmd) {
+    let usage = `${cmd['alias'][0]}`;
+
+    if (cmd['flags'].length > 0) {
+        for (let flag of cmd['flags'])
+            usage += ` ${flagFormat(flag)}`;
+    }
+
+    if (cmd['value'] !== null) {
+        usage += ` ${valueFormat(cmd['value'])}`;
+    }
+    return usage;
+}
+
+function flagFormat(flag, optionalFormat = true) {
+    let optionalStart = '', optionalEnd = '';
+    if (optionalFormat) {
+        optionalStart = (flag['optional']) ? '[' : '&#60;';
+        optionalEnd = (flag['optional']) ? ']' : '&#62;';
+    }
+
+    let usage = `${optionalStart}${flag['flag']}`;
+    if (flag['value'] !== null)
+        usage += ` ${valueFormat(flag['value'])}`;
+    usage += `${optionalEnd}`;
+    return usage;
+}
+
+function valueFormat(value) {
+    let type;
+    switch (value['type']) {
+        case 'string':
+            type = 'string';
+            break;
+        case 'number':
+            type = 'number';
+            break;
+        default:
+            type = null;
+    }
+    let flagStart = "";
+    let flagEnd = "";
+    if (type !== null) {
+        flagStart = `<${type}>`;
+        flagEnd = `</${type}>`;
+    }
+    return `&#60;${flagStart}${value['name']}${flagEnd}&#62;`;
 }
