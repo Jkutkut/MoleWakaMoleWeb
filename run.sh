64abe2c092a6f963058a365e023c1e2b542da9e6
Jkutkut <jorgeregonzalez@gmail.com>
2022-11-07 13:21:50 +0100

    run now showing QR code to connect to device

--- run.sh
+++ run.sh
@@ -2,7 +2,14 @@
 
 # TODO check .env file
 
+ip=$(hostname -I | awk '{print $1}')
 
+direction="$ip:3000/terminal"
+
+# TODO remove sudo usage
+sudo docker run -it --rm jkutkut/py-qr $direction
+
+npm start
 
 # sudo docker run --rm -it \
 # 	-p 3000:3000 \
