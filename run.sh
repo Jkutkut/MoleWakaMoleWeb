0d44b2ef67e329e3ac58ff6b556c05d62fdc5e04
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-06-19 11:28:50 +0200

    run script updated

--- run.sh
+++ run.sh
@@ -8,16 +8,41 @@ ip=$(hostname -I | awk '{print $1}')
 
 direction="http://$ip:3000/terminal"
 
+mode="Docker";
+
 if [ "$1" = "--qr" ]; then
-	# TODO remove sudo usage
-	sudo docker run -it --rm jkutkut/py-qr $direction
+	docker run -it --rm jkutkut/py-qr $direction
+	shift
 fi
+if [ "$1" = "--install" ]; then
+	docker run -it --rm --name molewakamole \
+		-p 3000:3000 \
+		-v $(pwd):/app -w /app \
+		--entrypoint=npm \
+	   node:current-alpine3.16 install
+	exit
+fi
+if [ "$1" = "--npm" ]; then
+	mode="npm"
+else
+	mode="Docker";
+fi
+
+
 
 echo "Running at:"
 echo "  $direction"
 echo "  http://localhost:3000/terminal"
 
-npm start
+if [ "$mode" = "npm" ]; then
+	npm start
+else
+	docker run -it --rm --name molewakamole \
+		-p 3000:3000 \
+		-v $(pwd):/app -w /app \
+		--entrypoint=npm \
+	   node:current-alpine3.16 run start
+fi
 
 # sudo docker run --rm -it \
 # 	-p 3000:3000 \
